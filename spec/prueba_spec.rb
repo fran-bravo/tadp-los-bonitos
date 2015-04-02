eeda4ed5dff8108e34e85561bebe6c7aac8a2c62
fran-bravo <fran_ase@hotmail.com>
2015-04-02 14:58:51 -0300

    Aplicado un mixin con sus respectivos tests y una clase universal

--- spec/prueba_spec.rb
+++ spec/prueba_spec.rb
@@ -18,4 +18,20 @@ describe 'Prueba de palomas' do
 
     expect(josefa.bailar_zumba()).to eq(15)
   end
+
+  it 'un birdman come 20 gramos y tiene 10 de energia' do
+    pedro = Birdman.new
+    pedro.energia=(10)
+    pedro.comer(20)
+
+    expect(pedro.energia()).to eq(130)
+  end
+
+  it 'un manbird come 20 gramos y tiene 10 de energia' do
+    pablo = Manbird.new
+    pablo.energia=(10)
+    pablo.comer(20)
+
+    expect(pablo.energia()).to eq(50)
+  end
 end
--- src/prueba.rb
+++ src/prueba.rb
@@ -1,16 +1,64 @@
-class Paloma
-  attr_accessor :energia, :vida
+################################### Clase Universal ###########################
 
-  def vitalidad()
-    return self.energia + self.vida
-  end
+class Animal
+  attr_accessor :energia, :vida
 
   def perder_energia(energy)
     self.energia -= energy
   end
 
+  def ganar_energia(energy)
+    self.energia += energy
+  end
+
+end
+
+################################### Modulos del Mixin ############################
+module Ave
+
+  def volar(unos_kms)
+    self.perder_energia(10 * unos_kms)
+  end
+
+  def comer(unos_gramos)
+    self.ganar_energia(2 * unos_gramos)
+  end
+
+end
+
+module Hombre
+
+  def comer(unos_gramos)
+    self.ganar_energia(6 * unos_gramos)
+  end
+
+end
+
+############################# Lo que genero le dio la vida a esto #############
+
+class Paloma < Animal
+  include Ave
+
+  def vitalidad()
+    return self.energia + self.vida
+  end
+
   def bailar_zumba()
     self.perder_energia(15)
   end
+
+end
+
+###################### Clases que aplican el Mixin #######################
+
+class Birdman < Animal
+  include Ave
+  include Hombre
+
+end
+
+class Manbird < Animal
+  include Hombre
+  include Ave
 
 end
