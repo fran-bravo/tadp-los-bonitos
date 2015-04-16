bc7559d8bd2f3d11b5b8ca6be6b8fa361cb6b9df
Dario Cappellini <dcappellini@hotmail.com.ar>
2015-04-15 23:11:00 -0300

    Agrego validación de que la cantidad de parámetros del bloque coincida con la cantidad de tipos que le paso al PartialBlock, agrego un test para verificar que intentar crear un PartialBlock que no lo cumpla rompe.

--- spec/PartialBlock_spec.rb
+++ spec/PartialBlock_spec.rb
@@ -3,6 +3,10 @@ require_relative '../src/PartialBlock'
 
 describe 'Pruebas sobre partial blocks'  do
 
+  it 'un bloque mal definido explota' do
+    expect{PartialBlock.new([]) do |argumento| end}.to raise_error(RuntimeError)
+  end
+
   it 'un bloque definido para string matchea con strings ' do
 
     helloBlock = PartialBlock.new([String]) do |who| #esto rompe porque manda un parámetro y según la consigna deberían ser dos, lo voy a preguntar en la lista
@@ -12,6 +16,7 @@ describe 'Pruebas sobre partial blocks'  do
     expect(helloBlock.matches("a")).to equal(true) #true
     expect(helloBlock.matches(1)).to equal(false) #false
     expect(helloBlock.matches("a", "b")).to equal(false) #false
+
 
   end
 end
--- src/PartialBlock.rb
+++ src/PartialBlock.rb
@@ -1,7 +1,11 @@
 class PartialBlock
 
   def initialize(tipos, &bloque) #tipos es un array de clases, &bloque es un bloque común y corriente (el do..end del ejemplo)
-
+    if tipos.length != bloque.arity
+      raise()
+      #throw new blah blah blah
+      #¿cómo se tira *bien* una excepción en Ruby?
+    end
   end
 
 
