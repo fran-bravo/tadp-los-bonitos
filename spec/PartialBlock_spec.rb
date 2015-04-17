2593b95a07aa081cb22178e2a7548c3068ae47ff
fran-bravo <fran_ase@hotmail.com>
2015-04-17 16:41:11 -0300

    Formalizada la excepcion en initialize y definido metodo matches con mas tests

--- spec/PartialBlock_spec.rb
+++ spec/PartialBlock_spec.rb
@@ -4,19 +4,29 @@ require_relative '../src/PartialBlock'
 describe 'Pruebas sobre partial blocks'  do
 
   it 'un bloque mal definido explota' do
-    expect{PartialBlock.new([]) do |argumento| end}.to raise_error(RuntimeError)
+    expect{PartialBlock.new([], Proc.new do |argumento| end)}.to raise_error(ArgumentException)
   end
 
   it 'un bloque definido para string matchea con strings ' do
 
-    helloBlock = PartialBlock.new([String]) do |who| #esto rompe porque manda un parámetro y según la consigna deberían ser dos, lo voy a preguntar en la lista
-      "Hello #{who}"
-    end
+    helloBlock = PartialBlock.new([String], Proc.new do |who| "Hello #{who}" end)
+
+    #esto rompe porque manda un parámetro y según la consigna deberían ser dos, lo voy a preguntar en la lista <- creo que con esto ya esta resuelto el comment
 
     expect(helloBlock.matches("a")).to equal(true) #true
     expect(helloBlock.matches(1)).to equal(false) #false
     expect(helloBlock.matches("a", "b")).to equal(false) #false
 
+  end
+
+  it 'un bloque parcial con mas de un parametro matchea correctamente' do
+
+    un_block = PartialBlock.new([Fixnum, Fixnum], Proc.new do |var1, var2| var1+var2 end)
+
+    expect(un_block.matches(2)).to equal(false) #false
+    expect(un_block.matches(1, "a")).to equal(false) #false
+    expect(un_block.matches(2, 4)).to equal(true) #true
 
   end
+
 end
--- src/PartialBlock.rb
+++ src/PartialBlock.rb
@@ -1,12 +1,36 @@
 class PartialBlock
+  attr_accessor :types, :block
 
-  def initialize(tipos, &bloque) #tipos es un array de clases, &bloque es un bloque común y corriente (el do..end del ejemplo)
+  def initialize(tipos, bloque) #tipos es un array de clases, &bloque es un bloque común y corriente (el do..end del ejemplo)
     if tipos.length != bloque.arity
-      raise()
+      raise(ArgumentException, "La cantidad de parametros no concuerdan con los requeridos") #Tambien se le puede agregar una condicion, agrego link en el drive
       #throw new blah blah blah
-      #¿cómo se tira *bien* una excepción en Ruby?
+      #¿cómo se tira *bien* una excepción en Ruby? | Confio en que este resuelto esto xD
+    elsif
+      self.types = tipos
+      self.block = bloque
     end
+
   end
 
+  def matches(*parametros)
+    if self.types.length != parametros.length
+      return false
+    end
+    i = 0
+    for tipo in self.types
+      if parametros[i].class != tipo
+        return false
+      end
+      i += 1
+    end
+    return true
+  end
+
+end
+
+#No encontre mejor lugar para definir esto por ahora
+
+class ArgumentException < StandardError
 
 end
