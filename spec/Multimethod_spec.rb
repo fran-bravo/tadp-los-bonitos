c975e8b712429bf6f1ad234bda18698b47e74680
Dario Cappellini <dcappellini@hotmail.com.ar>
2015-05-01 13:04:51 -0300

    Ordeno los tests sobre multimetodos. Separo los que prueban multimetodos sobre objetos de los que prueban multimetodos definidos en clases.

--- spec/Multimethod_spec.rb
+++ spec/Multimethod_spec.rb
@@ -25,96 +25,109 @@ describe 'Tests sobre multimethods' do
       end
     end
 
+  end
+
+
+
+
+  describe 'tests sobre definicion de multimetodos' do
+
+    it 'Se definió un multimétodo' do
+      expect(A.multimetodos.length).to eq(1)
     end
 
-  it 'Se definió un multimétodo' do
-    expect(A.multimetodos.length).to eq(1)
-  end
-
-  it 'Se definió un multimétodo con 4 definiciones parciales' do
-    expect(A.multimetodos[0].bloques_parciales.length).to eq(4)
-  end
-
-  it 'Se redefine un multimétodo, que sobreescribe otro con los mismos tipos' do
-    class A
-      partial_def :concat, [String, Integer] do |s1, n| "Hola Maiu!" end
+    it 'Se definió un multimétodo con 4 definiciones parciales' do
+      expect(A.multimetodos[0].bloques_parciales.length).to eq(4)
     end
 
-    expect(A.multimetodos.first.bloques_parciales.last.call("Juli", 9)).to eq("Hola Maiu!")
-    expect(A.multimetodos.first.bloques_parciales.length).to eq(4)
-  end
-
-  it 'Se puede preguntar por los multimétodos definidos en la clase' do
-    expect(A.multimethods).to eq([:concat])
-  end
-
-  it 'funciona el responds_to? para partial_def' do
-
-    expect(A.new.respond_to?(:concat)).to eq(true)
-
-  end
-
-  it 'funciona el responds_to? para un método común, en una clase que definió un partial method' do
-    expect(A.new.respond_to?(:to_s)).to eq(true)
-    # true, define el método normalmente
-  end
-
-  it 'funciona el responds_to? para un multimethod preguntando por una firma exactamente como una de las que tiene' do
-    expect(A.new.respond_to?(:concat, false, [String,String])).to eq(true)
-    # true, los tipos coinciden
-  end
-
-  it 'funciona el responds_to? para un multimethod preguntando por una firma que matchea por herencia con una de las que tiene' do
-    expect(A.new.respond_to?(:concat, false, [Integer,A])).to eq(true)
-    #esto matchea con el partial def que toma [Object, Object]
-  end
-
-  it 'puedo preguntar responds_to? por algo que es un método y no un multimétodo, y si especifico la firma no matchea' do
-   expect(A.new.respond_to?(:to_s, false, [String])).to eq(false)
-    # false, no es un multimethod
-  end
-
-  it 'puedo preguntar responds_to? por un multimethod que existe pero con una firma que no tiene, y da false' do
-   expect(A.new.respond_to?(:concat, false, [String,String,String])).to eq(false) # false, los tipos no coinciden
-  end
-
-  it 'responds_to? con true me da true si algún ancestro tiene el multimethod' do
-    module Coso
-      partial_def :hacer_cosa, [] do
-        return "que cosa che!"
+    it 'Se redefine un multimétodo, que sobreescribe otro con los mismos tipos' do
+      class A
+        partial_def :concat, [String, Integer] do |s1, n| "Hola Maiu!" end
       end
 
-      partial_def :hacer_cosa, [Object] do |object|
-        return "guarda que me dieron un parametro"
-      end
+      expect(A.multimetodos.first.bloques_parciales.last.call("Juli", 9)).to eq("Hola Maiu!")
+      expect(A.multimetodos.first.bloques_parciales.length).to eq(4)
     end
 
-    A.include(Coso)
-    a = A.new
-
-    expect(a.respond_to?(:hacer_cosa, true)).to eq(true)
-    expect(a.respond_to?(:hacer_cosa, true, 15)).to eq(true)
+    it 'Se puede preguntar por los multimétodos definidos en la clase' do
+      expect(A.multimethods).to eq([:concat])
+    end
 
   end
 
- # it 'responds_to? con true funciona cuando agrego un mixin directamente a un objeto' do
- #   a = String.new
- #   a.extend(Coso)
- #   expect(a.respond_to?(:hacer_cosa, true)).to eq(true)
- #   expect(a.respond_to?(:hacer_cosa, true, 15)).to eq(true)
- # end
-
-  #para este test de acá arriba habría que esperar a implementar herencia
 
 
-  it 'La distancia del parámetro se calcula correctamente' do
-    expect(Multimethod.new(:metodo).distancia_parametro(Numeric, 3)).to eq(2)
+  describe 'tests sobre respond_to?' do
+
+    it 'funciona el responds_to? para partial_def' do
+
+      expect(A.new.respond_to?(:concat)).to eq(true)
+
+    end
+
+    it 'funciona el responds_to? para un método común, en una clase que definió un partial method' do
+      expect(A.new.respond_to?(:to_s)).to eq(true)
+      # true, define el método normalmente
+    end
+
+    it 'funciona el responds_to? para un multimethod preguntando por una firma exactamente como una de las que tiene' do
+      expect(A.new.respond_to?(:concat, false, [String,String])).to eq(true)
+      # true, los tipos coinciden
+    end
+
+    it 'funciona el responds_to? para un multimethod preguntando por una firma que matchea por herencia con una de las que tiene' do
+      expect(A.new.respond_to?(:concat, false, [Integer,A])).to eq(true)
+      #esto matchea con el partial def que toma [Object, Object]
+    end
+
+    it 'puedo preguntar responds_to? por algo que es un método y no un multimétodo, y si especifico la firma no matchea' do
+     expect(A.new.respond_to?(:to_s, false, [String])).to eq(false)
+      # false, no es un multimethod
+    end
+
+    it 'puedo preguntar responds_to? por un multimethod que existe pero con una firma que no tiene, y da false' do
+     expect(A.new.respond_to?(:concat, false, [String,String,String])).to eq(false) # false, los tipos no coinciden
+    end
+
+    it 'responds_to? con true me da true si algún ancestro tiene el multimethod' do
+      module Coso
+        partial_def :hacer_cosa, [] do
+          return "que cosa che!"
+        end
+
+        partial_def :hacer_cosa, [Object] do |object|
+          return "guarda que me dieron un parametro"
+        end
+      end
+
+      A.include(Coso)
+      a = A.new
+
+      expect(a.respond_to?(:hacer_cosa, true)).to eq(true)
+      expect(a.respond_to?(:hacer_cosa, true, 15)).to eq(true)
+
+    end
+  end #end describe
+
+
+
+
+  describe 'tests sobre calculo de distancias' do
+
+    it 'La distancia del parámetro se calcula correctamente' do
+      expect(Multimethod.new(:metodo).distancia_parametro(Numeric, 3)).to eq(2)
+    end
+
+    it 'Se calcula correctamente la distancia de los parámetros' do
+      multimetodo = Multimethod.new(:metodo)
+      expect(multimetodo.distancia_parametros([Numeric, Numeric], [3, 3.0])).to eq(4)
+    end
+
   end
 
-  it 'Se calcula correctamente la distancia de los parámetros' do
-    multimetodo = Multimethod.new(:metodo)
-    expect(multimetodo.distancia_parametros([Numeric, Numeric], [3, 3.0])).to eq(4)
-  end
+
+
+
 
   it 'Se invoca un multimetodo' do
     expect(A.new.concat('hello', " world!")). to eq("hello world!")
@@ -123,22 +136,6 @@ describe 'Tests sobre multimethods' do
 
   end
 
-  it 'Funciona respond_to? cuando se agrega un multimetodo solo a una instancia' do
-    nueva_a = A.new
-
-    nueva_a.partial_def :multiplicar, [Integer, Integer] do |num1, num2| num1*num2 end #De momento no está implementado
-    expect(nueva_a.respond_to?(:multiplicar, [Integer, Integer])).to eq(true)
-  end
-
-=begin
-  it 'Funciona un multimétodo agregado sólo a una instancia' do
-    otra_a = A.new
-
-    otra_a.partial_def :hablar, [String] do  |s| return s end
-    expect(otra_a.hablar("ola ke ase")). to eq("ola ke ase")
-
-  end
-=end
 
   it 'Muestra la representacion de un multimethod' do
     expect(A.multimethod(:concat).class). to eq(Multimethod)
@@ -171,20 +168,6 @@ describe 'Tests sobre multimethods' do
 
     expect(dario.agregar_titulo).to eq("Ing. Dario") #si el test da verde me recibo?
 
-  end
-
-  it 'Un objeto entiende partial_def' do
-    a = A.new
-
-    expect(a.respond_to?(:partial_def)).to eq(true)
-  end
-
-  it 'Se define un partial_def en un objeto y solo para ese objeto' do
-    a = A.new
-    a.partial_def(:gritar, [String]) do |grito| grito + "!" end
-
-    expect(a.gritar("buu")).to eq("buu!")
-    expect(A.new.respond_to?(:gritar)).to eq(false)
   end
 
   end
---
+++ spec/object_multimethods_spec.rb
@@ -0,0 +1,58 @@
+require 'rspec'
+
+describe 'Tests sobre multimetodos definidos en objetos' do
+
+  class A
+
+  end
+
+  it 'Un objeto entiende partial_def' do
+    a = A.new
+
+    expect(a.respond_to?(:partial_def)).to eq(true)
+  end
+
+  it 'Se define un partial_def en un objeto y solo para ese objeto' do
+    a = A.new
+    a.partial_def(:gritar, [String]) do |grito| grito + "!" end
+
+    expect(a.gritar("buu")).to eq("buu!")
+    expect(A.new.respond_to?(:gritar)).to eq(false)
+  end
+
+
+  it 'Se define un partial_def en un objeto y no funciona para otro objeto' do
+    a = A.new
+    a.partial_def(:gritar, [String]) do |grito| grito + "!" end
+
+    expect(a.gritar("buu")).to eq("buu!")
+    expect{A.new.gritar("buu")}.to raise_error
+  end
+
+#  it 'Funciona respond_to? cuando se agrega un multimetodo solo a una instancia' do
+#    nueva_a = A.new
+#
+#    nueva_a.partial_def :multiplicar, [Integer, Integer] do |num1, num2| num1*num2 end #De momento no está implementado
+#    expect(nueva_a.respond_to?(:multiplicar, [Integer, Integer])).to eq(true)
+#  end
+
+#  begin
+#    it 'Funciona un multimétodo agregado sólo a una instancia' do
+#      otra_a = A.new
+#
+#      otra_a.partial_def :hablar, [String] do  |s| return s end
+#      expect(otra_a.hablar("ola ke ase")). to eq("ola ke ase")
+#
+#    end
+#end
+
+# it 'responds_to? con true funciona cuando agrego un mixin directamente a un objeto' do
+#   a = String.new
+#   a.extend(Coso)
+#   expect(a.respond_to?(:hacer_cosa, true)).to eq(true)
+#   expect(a.respond_to?(:hacer_cosa, true, 15)).to eq(true)
+# end
+
+#para este test de acá arriba habría que esperar a implementar herencia
+
+end
